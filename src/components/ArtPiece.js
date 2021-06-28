965b317bb092c5d6c08a45def9b3f25775af9089
knhim <nhimkap4@gmail.com>
2021-06-28 12:56:13 -0700

    added a modal and fixed some styling issues

--- src/components/ArtPiece.js
+++ src/components/ArtPiece.js
@@ -8,21 +8,29 @@ const ArtPiece = ({
   worktypes,
   dimensions,
   classification,
+  setArtModal,
 }) => {
   if (primaryimageurl === null) {
     return null;
   }
 
+  const handleClick = (e) => {
+    setArtModal(true);
+  };
+
   return (
     <>
-      <div className="border-2 border-gray-500 m-2">
-        <img src={primaryimageurl} alt="hello" />
-        <div className="p-2">
-          <h1>{title}</h1>
-          <p>{medium}</p>
-          <p>{dated}</p>
-          <p>{dimensions}</p>
+      <div className="border-2 border-gray-500 m-3" onClick={(e) => handleClick(e)}>
+        <div className="flex justify-center">
+          <img className="object-contain" src={primaryimageurl} alt={title} />
         </div>
+
+        {/* <div className="p-2">
+            <h1>{title}</h1>
+            <p>{medium}</p>
+            <p>{dated}</p>
+            <p>{dimensions}</p>
+          </div> */}
       </div>
     </>
   );
