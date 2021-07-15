22ef2b0f9836f882d4e764ca5578a2020a8d9c46
knhim <nhimkap4@gmail.com>
2021-07-14 23:18:31 -0700

    added max-height to art pieces so large pieces wouldn't take up the whole page

--- src/components/ArtPiece.js
+++ src/components/ArtPiece.js
@@ -25,36 +25,32 @@ const ArtPiece = ({
 
   if (display === false) {
     return (
-      <>
-        <div className="border-2 border-gray-600 mx-3 my-4" onClick={(e) => toggleCard(e)}>
-          <div className="flex justify-center">
-            <img className="object-contain" src={primaryimageurl} alt={title} />
-          </div>
+      <div className="border-2 border-gray-600 mx-3 my-4" onClick={(e) => toggleCard(e)}>
+        <div className="flex justify-center">
+          <img className="object-contain w-full max-h-96" src={primaryimageurl} alt={title} />
         </div>
-      </>
+      </div>
     );
   }
 
   return (
-    <>
-      <div className="border-2 border-gray-600 mx-3 my-4" onClick={(e) => toggleCard(e)}>
-        <div className="flex justify-center">
-          <img className="object-contain" src={primaryimageurl} alt={title} />
-        </div>
+    <div className="border-2 border-gray-600 mx-3 my-4" onClick={(e) => toggleCard(e)}>
+      <div className="flex justify-center">
+        <img className="object-contain w-full max-h-96" src={primaryimageurl} alt={title} />
+      </div>
 
-        <div className="p-2">
-          <h1>{title}</h1>
-          <p>Artist:</p>
-          <p>{medium}</p>
-          <p>{dated}</p>
-          <p>{dimensions}</p>
-        </div>
+      <div className="p-2">
+        <h1>{title}</h1>
+        <p>Artist:</p>
+        <p>{medium}</p>
+        <p>{dated}</p>
+        <p>{dimensions}</p>
+      </div>
 
-        {/* <div className="">
+      {/* <div className="">
           <Map latLng={latLng} />
         </div> */}
-      </div>
-    </>
+    </div>
   );
 };
 
